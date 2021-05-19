645288fe101fc89bb6731cdd54c9b4f397538631
tigocho <79485596+tigocho@users.noreply.github.com>
2021-05-19 10:58:54 -0500

    configuracion main
    main

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://localhost/appreport/appreport/';
+$config['base_url'] = 'http://localhost:9898/appreport/';
 
 /*
 |--------------------------------------------------------------------------
@@ -380,11 +380,11 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+/*$config['sess_save_path'] = NULL;*/
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-
+$config['sess_save_path'] = sys_get_temp_dir();
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
